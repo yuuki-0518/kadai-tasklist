d1cadb3440dd08d1c259c39fad3dbcef0d6abe01
yuki.hagino <yuki051803@gmail.com>
2020-08-11 02:39:32 +0000

    User

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,72 +8,24 @@ use App\User;
 
 class UsersController extends Controller
 {
-   /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-         $data = [];
-        if (\Auth::check()) { // 認証済みの場合
-            // 認証済みユーザを取得
-            $user = \Auth::user();
-            // ユーザの投稿の一覧を作成日時の降順で取得
-            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
+         // ユーザ一覧をidの降順で取得
+        $users = User::orderBy('id', 'desc')->paginate(10);
 
-            $data = [
-                'user' => $user,
-                'tasks' => $tasks,
-            ];
-        }
-
-        // Welcomeビューでそれらを表示
-        return view('welcome', $data);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        
-        // バリデーション
-        $request->validate([
-            'status' => 'required|max:10',  
-            'content' => 'required|max:255',
+        // ユーザ一覧ビューでそれを表示
+        return view('users.index', [
+            'users' => $users,
         ]);
-        
-         // 認証済みユーザ（閲覧者）の投稿として作成（リクエストされた値をもとに作成）
-        $request->user()->tasks()->create([
-            'content' => $request->content,
-        ]);
-        
-        // 前のURLへリダイレクトさせる
-        return back();
-        
+    
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show($id)
     {
         
-        //  // idの値でユーザを検索して取得
-        // $user = User::findOrFail($id);
-
-        // 関係するモデルの件数をロード
-        $user->loadRelationshipCounts();
-
-        // ユーザの投稿一覧を作成日時の降順で取得
-        $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
+         // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
 
         // // ユーザ詳細ビューでそれらを表示
         // return view('users.show', [
@@ -84,22 +36,5 @@ class UsersController extends Controller
     }
 
  
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        
-         // 認証済みユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
-        if (\Auth::id() === $task->user_id) {
-            $task->delete();
-        }
-        
-        // 前のURLへリダイレクトさせる
-        return back();
-    }
 }
 
