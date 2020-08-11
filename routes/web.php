129d338d366a6b657e41dcf410bdb9309cf7c2c7
yuki.hagino <yuki051803@gmail.com>
2020-08-11 06:05:05 +0000

    routes

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
